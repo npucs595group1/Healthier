e8a5b362b513c6c3365b51b9253a241104fcaad7
Yingwei Chen <josephchen6688@gmail.com>
2016-03-09 21:04:55 -0800

    3/9 latest version

--- app/src/main/java/group1/cs595/npu/healthier/MainActivity.java
+++ app/src/main/java/group1/cs595/npu/healthier/MainActivity.java
@@ -5,11 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.content.Intent;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
@@ -47,5 +62,26 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString() + "_Map";
         intent.putExtra(EXTRA_MESSAGE, message);*/
         startActivity(intent);
+    }
+    private Socket socket;
+    public void main_button_server_click(View view) {
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost("http://10.0.2.2:12001");
+        String myFeed = "http://10.0.2.2:12001";
+        try {
+            URL url = new URL(myFeed);
+
+            URLConnection connection = url.openConnection();
+            HttpURLConnection httpConnection = (HttpURLConnection)connection;
+
+            int responseCode = httpConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                InputStream in = httpConnection.getInputStream();
+                //[ ... Process the input stream as required ... ]
+            }
+        }
+        catch (MalformedURLException e) { }
+        catch (IOException e) { }
+
     }
 }
